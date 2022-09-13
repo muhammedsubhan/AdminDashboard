f64133efe8de990ccbaf68ec72a792b55b192a55
muhammedsubhan <muhammedsubhan692@gmail.com>
2022-09-13 07:51:05 -0700

    Dark Mode

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,12 +1,21 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
 
-
-menuBtn.addEventListener('click', () => {
-    sideMenu.classList.add('active');
+menuBtn.addEventListener("click", () => {
+  sideMenu.classList.add("active");
 });
 
-closeBtn.addEventListener('click', () => {
-    sideMenu.classList.remove('active');
+closeBtn.addEventListener("click", () => {
+  sideMenu.classList.remove("active");
+});
+
+// change theme dark
+
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-variables");
+
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
 });
